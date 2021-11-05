68e539297dab2c5c96000b3046206f14fb28134a
Danylo Havrylchenko <havrylchenko.d@gmail.com>
2021-11-05 07:47:20 +0200

    Added test controller for forcing task status message

---
+++ bot/src/main/java/com/community/tools/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.community.tools.controller;
+
+import com.community.tools.service.PublishWeekStatsService;
+import com.github.seratch.jslack.api.methods.SlackApiException;
+import java.io.IOException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/sendmessage")
+public class TestController {
+
+  @Autowired
+  PublishWeekStatsService service;
+
+  @GetMapping
+  public void sendMessage(){
+    service.publishTasksStatus();
+  }
+
+}
