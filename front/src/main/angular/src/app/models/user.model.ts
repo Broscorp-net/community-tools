2f3dba4f84f52d5872267f95127b234239d7bb42
Aleksandr Nimko <shurick2211@gmail.com>
2022-04-29 13:02:56 +0300

    merge conflicts 3

--- front/src/main/angular/src/app/models/user.model.ts
+++ front/src/main/angular/src/app/models/user.model.ts
@@ -3,27 +3,21 @@ import { UserTaskStatus } from "./user-task-status.model";
 export class User {
   gitName: string;
   dateRegistration: Date;
-  dateLastActivity: Date;
   karma: number;
   pointByTask: number;
   platformName: string;
   completedTasks: number;
   taskStatuses: UserTaskStatus[];
   totalPoints: number;
-  email: string;
 
-
-  constructor(gitName: string, dateRegistration: Date, dateLastActivity: Date, karma: number, pointByTask:number, platformName: string, completedTasks: number, taskStatuses: UserTaskStatus[], totalPoints: number, email:string) {
-
+  constructor(gitName: string, dateRegistration: Date, karma: number, pointByTask:number, platformName: string, completedTasks: number, taskStatuses: UserTaskStatus[], totalPoints: number) {
     this.gitName = gitName;
     this.dateRegistration = dateRegistration;
-    this.dateLastActivity = dateLastActivity;
     this.platformName = platformName;
     this.taskStatuses = taskStatuses;
     this.completedTasks = completedTasks;
     this.karma = karma;
     this.totalPoints = totalPoints;
-    this.email = email;
   }
 
 }
