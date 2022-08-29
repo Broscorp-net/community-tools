119dfc916aa6567299990a02affc2c78e25ef068
Volodymyr Tkach <vladimirv.tkach@gmail.com>
2022-08-29 15:51:29 +0300

    @DirtiesContext annotation has been added

--- bot/src/test/java/com/community/tools/service/EstimateTaskServiceTest.java
+++ bot/src/test/java/com/community/tools/service/EstimateTaskServiceTest.java
@@ -10,10 +10,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+@DirtiesContext
 @TestPropertySource(locations = "classpath:application-test.properties")
 @RunWith(SpringRunner.class)
 @SpringBootTest
