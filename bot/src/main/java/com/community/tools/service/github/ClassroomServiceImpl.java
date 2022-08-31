30cf0a14b8e8895b1b9a9a82e38352628445dffe
Volodymyr Tkach <vladimirv.tkach@gmail.com>
2022-08-31 14:10:13 +0300

    method addUserToOrganization() has been implemented

---
+++ bot/src/main/java/com/community/tools/service/github/ClassroomServiceImpl.java
@@ -0,0 +1,47 @@
+package com.community.tools.service.github;
+
+import com.community.tools.dto.UserDto;
+import java.time.Period;
+import java.util.List;
+import lombok.SneakyThrows;
+import org.kohsuke.github.GHOrganization;
+import org.kohsuke.github.GHTeam;
+import org.kohsuke.github.GHUser;
+import org.kohsuke.github.GitHub;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClassroomServiceImpl implements ClassroomService {
+
+  private final GitHub gitHub;
+  private final String organizationName;
+  private final String traineesTeamName;
+
+  @Autowired
+  public ClassroomServiceImpl(GitHub gitHub,
+      @Value("${github.organization}") String organizationName,
+      @Value("${github.team.trainees}") String traineesTeamName) {
+    this.gitHub = gitHub;
+    this.organizationName = organizationName;
+    this.traineesTeamName = traineesTeamName;
+  }
+
+  @SneakyThrows
+  @Override
+  public void addUserToOrganization(UserDto userDto) {
+    String gitName = userDto.getGitName();
+    GHUser user = gitHub.getUser(gitName);
+
+    GHOrganization organization = gitHub.getMyOrganizations().get(organizationName);
+    GHTeam traineesTeam = organization.getTeamByName(traineesTeamName);
+
+    traineesTeam.add(user);
+  }
+
+  @Override
+  public List<UserDto> getAllActiveUsers(Period period) {
+    return null;
+  }
+}
