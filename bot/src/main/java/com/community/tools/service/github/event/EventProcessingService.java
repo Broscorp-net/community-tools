4ac677d2a4f43a4c26c6a44e79bc45a19e1d2932
Volodymyr Tkach <vladimirv.tkach@gmail.com>
2022-08-31 17:11:05 +0300

    EventProcessingService has been implemented

---
+++ bot/src/main/java/com/community/tools/service/github/event/EventProcessingService.java
@@ -0,0 +1,17 @@
+package com.community.tools.service.github.event;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import org.json.JSONObject;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class EventProcessingService {
+
+  private List<GithubEventHandler> eventHandlers;
+
+  public void processEvent(JSONObject eventJson) {
+    eventHandlers.forEach(eventHandler -> eventHandler.handleEvent(eventJson));
+  }
+}
