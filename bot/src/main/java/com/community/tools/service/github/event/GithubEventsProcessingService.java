e43ec5f15663a85810cf9a3eb209bee507034499
Volodymyr Tkach <vladimirv.tkach@gmail.com>
2022-08-31 17:33:06 +0300

    class has been renamed

--- bot/src/main/java/com/community/tools/service/github/event/GithubEventsProcessingService.java
+++ bot/src/main/java/com/community/tools/service/github/event/GithubEventsProcessingService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 
 @AllArgsConstructor
 @Service
-public class EventProcessingService {
+public class GithubEventsProcessingService {
 
   private List<GithubEventHandler> eventHandlers;
 
