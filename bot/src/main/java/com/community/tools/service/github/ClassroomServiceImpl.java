f60b4b96ebbcf907968b5861172630c0a9e69a2c
Volodymyr Tkach <vladimirv.tkach@gmail.com>
2022-08-31 14:55:14 +0300

    getAllActiveUsers() method has been implemented

--- bot/src/main/java/com/community/tools/service/github/ClassroomServiceImpl.java
+++ bot/src/main/java/com/community/tools/service/github/ClassroomServiceImpl.java
@@ -1,10 +1,20 @@
 package com.community.tools.service.github;
 
 import com.community.tools.dto.UserDto;
+import java.io.IOException;
+import java.time.LocalDate;
 import java.time.Period;
+import java.time.ZoneId;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.stream.Collectors;
 import lombok.SneakyThrows;
 import org.kohsuke.github.GHOrganization;
+import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GHTeam;
 import org.kohsuke.github.GHUser;
 import org.kohsuke.github.GitHub;
@@ -19,16 +29,19 @@ public class ClassroomServiceImpl implements ClassroomService {
   private final String mainOrganizationName;
   private final String traineeshipOrganizationName;
   private final String traineesTeamName;
+  private final Set<String> taskRepositoryNamesPrefixes;
 
   @Autowired
   public ClassroomServiceImpl(GitHub gitHub,
       @Value("${github.organization.main}") String mainOrganizationName,
       @Value("${github.organization.traineeship}") String traineeshipOrganizationName,
-      @Value("${github.team.trainees}") String traineesTeamName) {
+      @Value("${github.team.trainees}") String traineesTeamName,
+      @Value("${github.task-repository-names.prefixes}") String[] taskRepositoryNamesPrefixes) {
     this.gitHub = gitHub;
     this.mainOrganizationName = mainOrganizationName;
     this.traineeshipOrganizationName = traineeshipOrganizationName;
     this.traineesTeamName = traineesTeamName;
+    this.taskRepositoryNamesPrefixes = new HashSet<>(Arrays.asList(taskRepositoryNamesPrefixes));
   }
 
   @SneakyThrows
@@ -43,8 +56,41 @@ public class ClassroomServiceImpl implements ClassroomService {
     traineesTeam.add(user);
   }
 
+  @SneakyThrows
   @Override
   public List<UserDto> getAllActiveUsers(Period period) {
-    return null;
+    Date startDate = Date.from(LocalDate
+        .now()
+        .minus(period)
+        .atStartOfDay(ZoneId.systemDefault()).toInstant());
+
+    GHOrganization organization = gitHub.getMyOrganizations().get(traineeshipOrganizationName);
+    List<GHRepository> activeRepositories = organization
+        .getRepositories()
+        .entrySet()
+        .stream()
+        .filter(entry -> {
+          String repositoryName = entry.getKey();
+          return taskRepositoryNamesPrefixes
+              .stream()
+              .anyMatch(repositoryName::startsWith);
+        })
+        .map(Entry::getValue)
+        .filter(repository -> {
+          try {
+            return repository.getUpdatedAt().after(startDate);
+          } catch (IOException e) {
+            throw new RuntimeException(e);
+          }
+        })
+        .collect(Collectors.toList());
+
+    return activeRepositories
+        .stream()
+        .map(GHRepository::getOwnerName)
+        .collect(Collectors.toSet())
+        .stream()
+        .map(ownerName -> new UserDto(null, ownerName))
+        .collect(Collectors.toList());
   }
 }
