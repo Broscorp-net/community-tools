cee11cbafd2df2c8162fe918079ce965cdfd713b
Volodymyr Tkach <vladimirv.tkach@gmail.com>
2022-09-01 11:34:37 +0300

    @RestController annotation has been added

--- bot/src/main/java/com/community/tools/controller/DeprecatedGitHubController.java
+++ bot/src/main/java/com/community/tools/controller/DeprecatedGitHubController.java
@@ -23,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Deprecated
+@RestController
 @RequiredArgsConstructor
 public class DeprecatedGitHubController {
 
