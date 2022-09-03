51f766db68e015bc3fa378705ea5a75dee64ecd0
Volodymyr Tkach <vladimirv.tkach@gmail.com>
2022-09-03 14:15:35 +0300

    RepositoryNameService has been implemented

---
+++ bot/src/main/java/com/community/tools/service/github/util/RepositoryNameService.java
@@ -0,0 +1,39 @@
+package com.community.tools.service.github.util;
+
+import com.community.tools.service.github.util.dto.ParsedRepositoryName;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RepositoryNameService {
+
+  private final Set<String> taskRepositoryNamesPrefixes;
+
+  @Autowired
+  public RepositoryNameService(@Value("${github.task-repository-names.prefixes}")
+      String[] taskRepositoryNamesPrefixes) {
+    this.taskRepositoryNamesPrefixes = new HashSet<>(Arrays.asList(taskRepositoryNamesPrefixes));
+  }
+
+  public boolean isPrefixedWithTaskName(String repositoryName) {
+    return taskRepositoryNamesPrefixes
+        .stream()
+        .anyMatch(repositoryName::startsWith);
+  }
+
+  public ParsedRepositoryName parseRepositoryName(String repositoryName) {
+    for (String prefix : taskRepositoryNamesPrefixes) {
+      if (repositoryName.startsWith(prefix)) {
+        String creatorGitName = repositoryName.replace(prefix, "");
+        return new ParsedRepositoryName(prefix, creatorGitName);
+      }
+    }
+
+    throw new IllegalArgumentException("Repository name is not prefixed with valid task name");
+  }
+
+}
