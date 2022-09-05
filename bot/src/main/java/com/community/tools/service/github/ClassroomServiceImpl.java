b7f11e962d4f6e6db15606c8a9c7f09ab8f3b279
Volodymyr Tkach <vladimirv.tkach@gmail.com>
2022-09-05 17:13:04 +0300

    ClassroomServiceImpl has been refactored

--- bot/src/main/java/com/community/tools/service/github/ClassroomServiceImpl.java
+++ bot/src/main/java/com/community/tools/service/github/ClassroomServiceImpl.java
@@ -2,8 +2,8 @@ package com.community.tools.service.github;
 
 import static java.util.Comparator.comparing;
 import static java.util.stream.Collectors.groupingBy;
-import static java.util.stream.Collectors.maxBy;
 
+import com.community.tools.dto.GithubRepositoryDto;
 import com.community.tools.dto.GithubUserDto;
 import com.community.tools.service.github.util.RepositoryNameService;
 import com.community.tools.service.github.util.dto.ParsedRepositoryName;
@@ -11,12 +11,14 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.time.Period;
 import java.time.ZoneId;
-import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 import java.util.stream.Collectors;
 import lombok.SneakyThrows;
+import org.kohsuke.github.GHLabel;
 import org.kohsuke.github.GHOrganization;
 import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GHTeam;
@@ -67,17 +69,8 @@ public class ClassroomServiceImpl implements ClassroomService {
         .minus(period)
         .atStartOfDay(ZoneId.systemDefault()).toInstant());
 
-    Comparator<GHRepository> repositoriesComparator = comparing(repository -> {
-      try {
-        return repository.getUpdatedAt();
-      } catch (IOException e) {
-        throw new RuntimeException(e);
-      }
-    });
-
     GHOrganization organization = gitHub.getMyOrganizations().get(traineeshipOrganizationName);
-
-    return organization
+    Map<String, List<GHRepository>> activeUserRepositories = organization
         .getRepositories()
         .entrySet()
         .stream()
@@ -94,25 +87,87 @@ public class ClassroomServiceImpl implements ClassroomService {
           ParsedRepositoryName parsedName = repositoryNameService.parseRepositoryName(
               repository.getName());
           return parsedName.getCreatorGitName();
-        }, maxBy(repositoriesComparator)))
+        }));
+
+    return activeUserRepositories
         .entrySet()
         .stream()
-        .map(entry -> buildGithubUserDto(entry.getKey(), entry.getValue().get()))
+        .map(entry -> buildGithubUserDto(entry.getKey(), entry.getValue()))
         .collect(Collectors.toList());
   }
 
-  private GithubUserDto buildGithubUserDto(String ownerName, GHRepository repository) {
-    try {
-      LocalDate lastCommit = repository
-          .getUpdatedAt()
-          .toInstant()
-          .atZone(ZoneId.systemDefault())
-          .toLocalDate();
+  private GithubUserDto buildGithubUserDto(String creatorGitName, List<GHRepository> repositories) {
+    repositories.sort(comparing(repository -> {
+      try {
+        return repository.getUpdatedAt();
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    }));
 
-      return new GithubUserDto(ownerName, lastCommit);
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
+    return GithubUserDto.builder()
+        .gitName(creatorGitName)
+        .lastCommit(getUpdatedAt(repositories.get(0)))
+        .repositories(repositories
+            .stream()
+            .map(this::buildGithubRepositoryDto)
+            .collect(Collectors.toList()))
+        .build();
+  }
+
+  @SneakyThrows
+  private GithubRepositoryDto buildGithubRepositoryDto(GHRepository repository) {
+    String repositoryName = repository.getName();
+    ParsedRepositoryName parsedRepositoryName = repositoryNameService.parseRepositoryName(
+        repository.getName());
+
+    return GithubRepositoryDto.builder()
+        .repositoryName(repositoryName)
+        .taskName(parsedRepositoryName.getTaskName())
+        .lastBuildStatus(getLastBuildStatus(repository))
+        .labels(getLabels(repository))
+        .createdAt(getCreatedAt(repository))
+        .updatedAt(getUpdatedAt(repository))
+        .build();
+  }
+
+  @SneakyThrows
+  private String getLastBuildStatus(GHRepository repository) {
+    return repository
+        .getWorkflow("classroom.yaml")
+        .listRuns()
+        .toList()
+        .get(0)
+        .getConclusion()
+        .toString();
+  }
+
+  @SneakyThrows
+  private Set<String> getLabels(GHRepository repository) {
+    return repository
+        .getPullRequest(0)
+        .getLabels()
+        .stream()
+        .map(GHLabel::getName)
+        .collect(Collectors.toSet());
+  }
+
+  @SneakyThrows
+  private LocalDate getCreatedAt(GHRepository repository) {
+    return repository
+        .getCreatedAt()
+        .toInstant()
+        .atZone(ZoneId.systemDefault())
+        .toLocalDate();
+  }
+
+  @SneakyThrows
+  private LocalDate getUpdatedAt(GHRepository repository) {
+    return repository
+        .getUpdatedAt()
+        .toInstant()
+        .atZone(ZoneId.systemDefault())
+        .toLocalDate();
   }
 }
 
