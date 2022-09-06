6bd4eca10c8daeab9a1da82de0dd3115cbd8f533
rwwwx <vlad.matiukhin@gmail.com>
2022-09-06 14:12:19 +0300

    reworked TaskStatusService and LeaderBoardController

--- bot/src/main/java/com/community/tools/controller/TaskStatusController.java
+++ bot/src/main/java/com/community/tools/controller/TaskStatusController.java
@@ -1,15 +1,18 @@
 package com.community.tools.controller;
 
+import com.community.tools.dto.GithubRepositoryDto;
 import com.community.tools.dto.GithubUserDto;
 import com.community.tools.service.TaskStatusService;
+import java.time.Period;
 import java.util.List;
-import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,6 +20,8 @@ public class TaskStatusController {
 
   @Value("${tasksForUsers}")
   private Set<String> tasksForUsers;
+  @Value("${defaultNumberOfDaysForStatistic}")
+  private Integer defaultNumberOfDays;
 
   private final TaskStatusService taskStatusService;
 
@@ -24,13 +29,25 @@ public class TaskStatusController {
     this.taskStatusService = taskStatusService;
   }
 
+  @GetMapping("/taskStatus/getAll")
+  public ResponseEntity<List<GithubUserDto>> getAllTaskStatuses(
+      @RequestParam(required = false) Optional<Integer> periodInDays) {
+    return new ResponseEntity<>(
+        taskStatusService.getAll(Period.ofDays(periodInDays.orElse(defaultNumberOfDays))),
+        HttpStatus.OK);
+  }
+
   @GetMapping("/taskStatus/getTaskForName/{taskName}")
-  public ResponseEntity<List<GithubUserDto>> getUsersForTask(@PathVariable String taskName) {
+  public ResponseEntity<List<GithubRepositoryDto>> getTaskStatusesForName(@PathVariable String taskName,
+      @RequestParam(required = false) Optional<Integer> days) {
 
     if (tasksForUsers.contains(taskName)) {
-      return new ResponseEntity<>(taskStatusService.getTasksForUsers(taskName),
+      return new ResponseEntity<>(
+          taskStatusService.getTaskStatusesForName(taskName,
+              Period.ofDays(days.orElse(defaultNumberOfDays))),
           HttpStatus.OK);
     } else {
+      //TODO throw exception and add AOP handler
       return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
--- bot/src/main/java/com/community/tools/service/TaskStatusService.java
+++ bot/src/main/java/com/community/tools/service/TaskStatusService.java
@@ -1,7 +1,10 @@
 package com.community.tools.service;
 
+import com.community.tools.dto.GithubRepositoryDto;
 import com.community.tools.dto.GithubUserDto;
 import com.community.tools.service.github.ClassroomServiceImpl;
+import java.time.Period;
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.stereotype.Service;
 
@@ -14,8 +17,20 @@ public class TaskStatusService {
     this.classroomService = classroomService;
   }
 
-  public List<GithubUserDto> getTasksForUsers(String taskName) {
-    return null;
+  public List<GithubUserDto> getAll(Period period) {
+    return classroomService.getAllActiveUsers(period);
+  }
+
+  public List<GithubRepositoryDto> getTaskStatusesForName(String taskName, Period period) {
+    List<GithubRepositoryDto> result = new ArrayList<>();
+    classroomService.getAllActiveUsers(period).forEach(
+        githubUserDto -> githubUserDto.getRepositories().forEach(githubRepositoryDto -> {
+          if (githubRepositoryDto.getTaskName().equals(taskName)) {
+            result.add(githubRepositoryDto);
+          }
+        })
+    );
+    return result;
   }
 
 }
