ee6d0b8d7707459a3092dde7c0df78c33908fea4
Larisa Pomidor <nikakinikakin@gmail.com>
2023-08-22 13:34:18 +0300

    Added scheduled check and processing of changes in the classroom

--- bot/src/main/java/com/community/tools/service/github/ClassroomService.java
+++ bot/src/main/java/com/community/tools/service/github/ClassroomService.java
@@ -1,6 +1,8 @@
 package com.community.tools.service.github;
 
 import com.community.tools.dto.GithubUserDto;
+
+import java.io.IOException;
 import java.time.Period;
 import java.util.List;
 
@@ -9,5 +11,7 @@ public interface ClassroomService {
   void addUserToTraineesTeam(String gitName);
 
   List<GithubUserDto> getAllActiveUsers(Period period);
+
+  void notificationHandler() throws IOException;
 
 }
