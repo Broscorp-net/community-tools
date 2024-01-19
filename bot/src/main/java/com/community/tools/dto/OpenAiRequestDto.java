4a3715e865bd54493a13307a299f96b1dba2b819
Maksym Musiienko <maxymvm@gmail.com>
2024-01-19 14:06:47 +0200

    added lombok annotations in OpenAiRequestDto

--- bot/src/main/java/com/community/tools/dto/OpenAiRequestDto.java
+++ bot/src/main/java/com/community/tools/dto/OpenAiRequestDto.java
@@ -2,39 +2,28 @@ package com.community.tools.dto;
 
 import java.util.Collections;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class OpenAiRequestDto {
   private String model;
   private List<Message> messages;
 
   public OpenAiRequestDto(String model, String prompt) {
     this.model = model;
-    this.messages = Collections.singletonList(new OpenAiRequestDto.Message("user", prompt));
+    this.messages = Collections.singletonList(new Message("user", prompt));
   }
 
-  public String getModel() {
-    return model;
-  }
-
-  public List<Message> getMessages() {
-    return messages;
-  }
-
+  @Getter
+  @Setter
+  @AllArgsConstructor
   public static class Message {
     private String role;
     private String content;
-
-    public Message(String role, String content) {
-      this.role = role;
-      this.content = content;
-    }
-
-    public String getRole() {
-      return role;
-    }
-
-    public String getContent() {
-      return content;
-    }
   }
 }
