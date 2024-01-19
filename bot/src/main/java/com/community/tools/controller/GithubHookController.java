01fa6a244b4b12d971aaca7a192037ce21bc1385
Vsevolod Kalinbet <vsevolod180203@gmail.com>
2024-01-19 13:34:01 +0200

    clean up

--- bot/src/main/java/com/community/tools/controller/GithubHookController.java
+++ bot/src/main/java/com/community/tools/controller/GithubHookController.java
@@ -2,7 +2,6 @@ package com.community.tools.controller;
 
 import com.community.tools.service.github.event.GithubEventsProcessingService;
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.json.JSONObject;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/gitHook")
-@Slf4j
 public class GithubHookController {
 
   private final GithubEventsProcessingService eventsProcessingService;
