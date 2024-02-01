4e880f0be5c14ab23537d93acb5b538d9e0544dc
Maksym Musiienko <maxymvm@gmail.com>
2024-02-01 15:16:16 +0200

    resolving merge conflicts

--- bot/src/main/java/com/community/tools/model/Mentors.java
+++ bot/src/main/java/com/community/tools/model/Mentors.java
@@ -1,24 +1,27 @@
 package com.community.tools.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
 @AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "mentors")
 public class Mentors {
 
   @Id
   private String gitNick;
+  @Column(unique = true)
+  private String discordName;
   //replace too discord
   private String slackId;
-
-  public Mentors() {}
 
 }
