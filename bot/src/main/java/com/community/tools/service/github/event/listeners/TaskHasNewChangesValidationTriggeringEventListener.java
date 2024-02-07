22e1018ff34f466979c47219ba971a7a09dfe181
Vsevolod Kalinbet <vsevolod180203@gmail.com>
2024-02-07 19:12:55 +0200

    trigger ai validation on task change

---
+++ bot/src/main/java/com/community/tools/service/github/event/listeners/TaskHasNewChangesValidationTriggeringEventListener.java
@@ -0,0 +1,27 @@
+package com.community.tools.service.github.event.listeners;
+
+import com.community.tools.dto.events.tasks.TaskStatusChangeEventDto;
+import com.community.tools.model.TaskStatus;
+import com.community.tools.service.github.PullRequestValidationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class TaskHasNewChangesValidationTriggeringEventListener implements
+    TaskStatusChangeEventListener {
+
+  private final PullRequestValidationService pullRequestValidationService;
+  @Value("${github.org.name}")
+  private String githubOrgName;
+
+  @Override
+  public void handleEvent(TaskStatusChangeEventDto event) {
+    if (event.isWithNewChanges() && event.getTaskStatus()
+        .equals(TaskStatus.READY_FOR_REVIEW)) {
+      pullRequestValidationService.validatePullRequest(githubOrgName + "/"
+          + event.getTaskName() + "-" + event.getTraineeGitName());
+    }
+  }
+}
